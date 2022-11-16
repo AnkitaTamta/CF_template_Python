8a396fdb08e754b2b21935b8cdae8b890fd23bb3
AnkitaTamta <118276846+AnkitaTamta@users.noreply.github.com>
2022-11-16 21:34:01 +0530

    Added code for protected subnets

--- build-cloudformation-vpc.py
+++ build-cloudformation-vpc.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
-#This Python script will create Cloudformation stack in JSON using troposphere library.
+
+##########################################################################################
+#  This Python script will create Cloudformation stack in JSON using troposphere library.#
+#  Account will have a 3 VPC ( Dev, Stage, Prod)                                         #
+#  Account will have multi-az subnets.                                                   #
+#																						 #
+#  Private- No internet access                                                           #
+#  Public- Outbound internet access                                                      #
+#  Protected- Outbound internet access via NAT                                           #
+##########################################################################################
 
 from troposphere import (
     GetAZs,
@@ -16,6 +25,7 @@ from troposphere.ec2 import (
     VPC,
     InternetGateway,
     NetworkAcl,
+    NatGateway,
     NetworkAclEntry,
     NetworkInterfaceProperty,
     PortRange,
@@ -28,140 +38,215 @@ from troposphere.ec2 import (
 )
 from troposphere.policies import CreationPolicy, ResourceSignal
 
-VPC_NETWORK   = "10.1.0.0/18"
-VPC_PRIVATE_1 = "10.1.0.0/20"
-VPC_PRIVATE_2 = "10.1.16.0/20"
-VPC_PUBLIC_1  = "10.1.32.0/20"
-VPC_PUBLIC_2  = "10.1.48.0/20"
+# DEV VPC Variables
+
+DEV_VPC_NETWORK      = "10.0.0.0/24"
+DEV_VPC_PRIVATE_1    = "10.0.0.0/27"
+DEV_VPC_PRIVATE_2    = "10.0.0.32/27"
+DEV_VPC_PUBLIC_1     = "10.0.0.64/27"
+DEV_VPC_PUBLIC_2     = "10.0.0.96/27"
+DEV_VPC_PROTECTED_1  = "10.0.0.128/27"
+DEV_VPC_PROTECTED_2  = "10.0.0.160/27"
 
 t = Template()
 
 t.set_version("2010-09-09")
 
-t.set_description("Stack for creating a basic VPC with private and public Subnets")
+t.set_description("Stack for creating a VPC with private subnets, public subnets and protected subnets")
 
 
-# VPC Creation
+# DEV VPC Creation
 
-VPC = t.add_resource(VPC(
-      "VPC",
-        CidrBlock=VPC_NETWORK,
+devvpc = t.add_resource(VPC(
+        "DEVVPC",
+        CidrBlock=DEV_VPC_NETWORK,
         InstanceTenancy="default",
         EnableDnsSupport=True,
-        Tags=Tags(Name="TestVPC",Application=Ref("AWS::StackId"))
+        Tags=Tags(Name="DEV_VPC",Application=Ref("AWS::StackId"))
         )
 )
 
-# Private routing table
+# Dev Private routing table
 
-privaterouteTable = t.add_resource(RouteTable(
-        "PrivateRouteTable",
-        VpcId=Ref(VPC),
-        Tags=Tags(Name="Private_Route_Table",Application=Ref("AWS::StackId"))
+devprivaterouteTable = t.add_resource(RouteTable(
+        "DevPrivateRouteTable",
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="Dev_Private_Route_Table",Application=Ref("AWS::StackId"))
         )
 )
 
-# private subnetworks
+# Dev private subnetworks
 
-subnetPrivate1 = t.add_resource(Subnet(
-        "privatesubnet1",
+devsubnetPrivate1 = t.add_resource(Subnet(
+        "devprivatesubnet1",
         AvailabilityZone=Select(0, GetAZs()),
-        CidrBlock=VPC_PRIVATE_1,
-        VpcId=Ref(VPC),
-        Tags=Tags(Name="Private_Subnet_1",Application=Ref("AWS::StackId"))
+        CidrBlock=DEV_VPC_PRIVATE_1,
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="Dev_Private_Subnet_1",Application=Ref("AWS::StackId"))
         )
 )
-
 
 t.add_resource(SubnetRouteTableAssociation(
-        "PrivateSubnet1RouteTable",
-        RouteTableId=Ref(privaterouteTable),
-        SubnetId=Ref(subnetPrivate1)
+        "DevPrivateSubnet1RouteTable",
+        RouteTableId=Ref(devprivaterouteTable),
+        SubnetId=Ref(devsubnetPrivate1)
         )
 )
 
-subnetPrivate2 = t.add_resource(Subnet(
-        "privatesubnet2",
+devsubnetPrivate2 = t.add_resource(Subnet(
+        "devprivatesubnet2",
         AvailabilityZone=Select(1, GetAZs()),
-        CidrBlock=VPC_PRIVATE_2,
-        VpcId=Ref(VPC),
-        Tags=Tags(Name="Private_Subnet_2",Application=Ref("AWS::StackId"))
+        CidrBlock=DEV_VPC_PRIVATE_2,
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="Dev_Private_Subnet_2",Application=Ref("AWS::StackId"))
         )
 )
-
 
 t.add_resource(SubnetRouteTableAssociation(
-        "PrivateSubnet2RouteTable",
-        RouteTableId=Ref(privaterouteTable),
-        SubnetId=Ref(subnetPrivate2)
+        "DevPrivateSubnet2RouteTable",
+        RouteTableId=Ref(devprivaterouteTable),
+        SubnetId=Ref(devsubnetPrivate2)
         )
 )
 
 
-# Internet gateway
+# Dev Internet gateway
 
-internetGateway = t.add_resource(InternetGateway(
-        "InternetGateway",
-        Tags=Tags(Name="Test_IGW",Application=Ref("AWS::StackId"))
+devinternetGateway = t.add_resource(InternetGateway(
+        "DevInternetGateway",
+        Tags=Tags(Name="Dev_IGW",Application=Ref("AWS::StackId"))
         )
 )
 
 gatewayAttachment = t.add_resource(VPCGatewayAttachment(
-        "InternetGatewayAttachment",
-        InternetGatewayId=Ref(internetGateway),
-        VpcId=Ref(VPC)
+        "DevInternetGatewayAttachment",
+        InternetGatewayId=Ref(devinternetGateway),
+        VpcId=Ref(devvpc)
         )
 )
 
-# Public routing table
+# Dev Public routing table
 
-publicRouteTable = t.add_resource(RouteTable(
-        "PublicRouteTable",
-        VpcId=Ref(VPC),
-        Tags=Tags(Name="Public_Route_Table",Application=Ref("AWS::StackId"))
+devpublicRouteTable = t.add_resource(RouteTable(
+        "DevPublicRouteTable",
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="Dev_Public_Route_Table",Application=Ref("AWS::StackId"))
         )
 )
 
-internetRoute = t.add_resource(Route(
-        "RouteToInternet",
+devinternetRoute = t.add_resource(Route(
+        "DevRouteToInternet",
         DestinationCidrBlock="0.0.0.0/0",
-        GatewayId=Ref(internetGateway),
-        RouteTableId=Ref(publicRouteTable),
+        GatewayId=Ref(devinternetGateway),
+        RouteTableId=Ref(devpublicRouteTable),
         DependsOn=gatewayAttachment.title
         )
 )
 
-# public subnetworks
+# Dev public subnetworks
 
-subnetPublic1 = t.add_resource(Subnet(
-        "publicsubnet1",
+devsubnetPublic1 = t.add_resource(Subnet(
+        "devpublicsubnet1",
         AvailabilityZone=Select(0, GetAZs()),
-        CidrBlock=VPC_PUBLIC_1,
-        VpcId=Ref(VPC),
-        Tags=Tags(Name="Public_Subnet_1",Application=Ref("AWS::StackId"))
+        CidrBlock=DEV_VPC_PUBLIC_1,
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="Dev_Public_Subnet_1",Application=Ref("AWS::StackId"))
         )
 )
 
 t.add_resource(SubnetRouteTableAssociation(
-        "PublicSubnet1RouteTable",
-        RouteTableId=Ref(publicRouteTable),
-        SubnetId=Ref(subnetPublic1)
+        "DevPublicSubnet1RouteTable",
+        RouteTableId=Ref(devpublicRouteTable),
+        SubnetId=Ref(devsubnetPublic1)
         )
 )
 
-subnetPublic2 = t.add_resource(Subnet(
-        "publicsubnet2",
+devsubnetPublic2 = t.add_resource(Subnet(
+        "devpublicsubnet2",
         AvailabilityZone=Select(1, GetAZs()),
-        CidrBlock=VPC_PUBLIC_2,
-        VpcId=Ref(VPC),
-        Tags=Tags(Name="Public_Subnet_2",Application=Ref("AWS::StackId"))
+        CidrBlock=DEV_VPC_PUBLIC_2,
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="Dev_Public_Subnet_2",Application=Ref("AWS::StackId"))
         )
 )
 
 t.add_resource(SubnetRouteTableAssociation(
-        "PublicSubnet2RouteTable",
-        RouteTableId=Ref(publicRouteTable),
-        SubnetId=Ref(subnetPublic2)
+        "DevPublicSubnet2RouteTable",
+        RouteTableId=Ref(devpublicRouteTable),
+        SubnetId=Ref(devsubnetPublic2)
+        )
+)
+
+# Dev protected NAT gateway
+
+devnatgtw = t.add_resource(VPCGatewayAttachment(
+        "devNatgtw",
+        VpcId=Ref(devvpc),
+        InternetGatewayId=Ref(devinternetGateway),
+    )
+)
+
+devnateip = t.add_resource(EIP(
+        "NatEip",
+        Domain="devvpc",
+    )
+)
+
+devnat = t.add_resource(NatGateway(
+        "DevNat",
+        AllocationId=GetAtt(devnateip, "AllocationId"),
+        SubnetId=Ref(devsubnetPublic1),
+    )
+)
+
+# Dev Protected routing table
+
+devprotectedrouteTable = t.add_resource(RouteTable(
+        "DevProtectedRouteTable",
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="DEV_Protected_Route_Table",Application=Ref("AWS::StackId"))
+        )
+)
+
+t.add_resource(Route(
+        "DevNatRoute",
+        RouteTableId=Ref(devprotectedrouteTable),
+        DestinationCidrBlock="0.0.0.0/0",
+        NatGatewayId=Ref(devnat),
+    )
+)
+
+# Dev protected subnetworks
+
+devsubnetProtected1 = t.add_resource(Subnet(
+        "devprotectedsubnet1",
+        AvailabilityZone=Select(0, GetAZs()),
+        CidrBlock=DEV_VPC_PROTECTED_1,
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="Dev_Protected_Subnet_1",Application=Ref("AWS::StackId"))
+        )
+)
+
+t.add_resource(SubnetRouteTableAssociation(
+        "DevProtectedSubnet1RouteTable",
+        RouteTableId=Ref(devprotectedrouteTable),
+        SubnetId=Ref(devsubnetProtected1)
+        )
+)
+
+devsubnetProtected2 = t.add_resource(Subnet(
+        "devprotectedsubnet2",
+        AvailabilityZone=Select(1, GetAZs()),
+        CidrBlock=DEV_VPC_PROTECTED_2,
+        VpcId=Ref(devvpc),
+        Tags=Tags(Name="Dev_Protected_Subnet_2",Application=Ref("AWS::StackId"))
+        )
+)
+
+t.add_resource(SubnetRouteTableAssociation(
+        "DevProtectedSubnet2RouteTable",
+        RouteTableId=Ref(devprotectedrouteTable),
+        SubnetId=Ref(devsubnetProtected2)
         )
 )
 
